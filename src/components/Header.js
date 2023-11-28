20bb7fe3284873cafc0a9832c5ef0ad5c5f94784
Hajin Lee <betterthanhajin@icloud.com>
2023-11-28 16:44:55 +0900

    add motion

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
+import React from "react";
+
 export function Header() {
   return (
     <div
       style={{
         backgroundColor: "black",
         marginLeft: "100px",
-        marginRight: "100px"
+        marginRight: "100px",
+        width: "100%",
+        margin: "0 auto",
+        position: "fixed" /* 헤더를 고정시킵니다. */,
+        top: 0 /* 화면 상단에 고정됩니다. */,
+        width: "100%" /* 가로 너비를 꽉 채웁니다. */,
+        boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)" /* 그림자 효과 */,
+        zIndex: 100 /* 다른 요소 위에 표시될 수 있도록 z-index 설정 */,
       }}
     >
       <ul
@@ -12,7 +21,9 @@ export function Header() {
           padding: "20px",
           color: "white",
           display: "flex",
-          justifyContent: "space-between"
+          justifyContent: "space-between",
+          maxWidth: "800px",
+          margin: "0 auto",
         }}
       >
         <li>HOME</li>
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,6 +7,8 @@ import {
 } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import React, { useEffect, useRef } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { throttle } from "lodash";
 import slide01 from "../image/slide_01.png";
 import slide02 from "../image/slide_02.png";
 import slide03 from "../image/slide_03.png";
@@ -14,9 +16,57 @@ import slide04 from "../image/slide_04.png";
 import starFrame from "../image/starFrame.svg";
 
 export function LandingPage() {
+  const elementRef = useRef(null);
+  setTimeout(() => {
+    let circlewrap = document.getElementById("circleEvent");
+    let circles = document.querySelectorAll(".circle0");
+    let random = Math.floor(Math.random() * 180);
+    let random2 = Math.floor(Math.random() * 480);
+    const mouseX = random;
+    const mouseY = random2;
+
+    for (let i = 0; i < circles.length; i++) {
+      circles[i].style.left = mouseX + "px";
+      circles[i].style.top = mouseY + "px";
+      circles[i].style.transition = "1s";
+    }
+    let j = 0;
+    circlewrap.addEventListener("mouseout", function () {
+      let random = Math.floor(Math.random() * 600 + 100);
+      let random2 = Math.floor(Math.random() * 480 + 200);
+      const mouseX = random;
+      const mouseY = random2;
+      circles[j].style.left = mouseX + "px";
+      circles[j].style.top = mouseY + "px";
+      circles[j].style.transition = "2.5s";
+      j++;
+      if (j == circles.length) {
+        j = 0;
+      }
+    });
+  }, 1000);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // 요소가 화면에 나타나면 CSS 클래스를 추가하여 애니메이션을 실행합니다.
+          entry.target.classList.add("typing-text");
+        }
+      });
+    });
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
+      }
+    };
+  }, []);
   return (
     <>
-      ㄴ
       <div id="scrollContainer">
         <section style={{ position: "relative" }}>
           <div className="o-container">
@@ -28,6 +78,7 @@ export function LandingPage() {
                 zIndex: 10,
                 fontSize: "7vw",
                 color: "white",
+                padding: "0 20",
               }}
             >
               VISUAL
@@ -40,12 +91,13 @@ export function LandingPage() {
                 width: "100%",
                 height: "100%",
                 position: "absolute",
+                backgroundColor: "yellow",
                 top: "0",
                 left: "0",
                 right: "0",
               }}
             >
-              <video
+              {/* <video
                 controls="controls"
                 muted="muted"
                 autoPlay
@@ -63,7 +115,7 @@ export function LandingPage() {
                   src="https://dev.zipshowkorea.com/assets/Zipshowkorea2023.5508ea02.mp4"
                   type="video/mp4"
                 ></source>
-              </video>
+              </video> */}
             </div>
             <header className="c-header" id="header">
               <div
@@ -105,7 +157,7 @@ export function LandingPage() {
 
         <section style={{ width: "100%", position: "relative" }}>
           <div className="o-container" style={{ position: "relative" }}>
-            <div style={{ paddingLeft: 10, paddingRight: 10 }}>
+            <div style={{ paddingLeft: 10, paddingRight: 10, marginTop: 100 }}>
               <span style={{ color: "white", fontSize: 45 }}>
                 ## OUR
                 <br />
@@ -167,7 +219,6 @@ export function LandingPage() {
                 direction={"horizontal"}
                 slidesPerView={1}
                 spaceBetween={30}
-                mousewheel={true}
                 pagination={{
                   clickable: true,
                 }}
@@ -233,8 +284,11 @@ export function LandingPage() {
           </div>
         </section>
 
-        <section style={{ width: "100%", position: "relative" }}>
-          <div className="o-container" style={{ position: "relative" }}>
+        <section style={{ width: "100%", position: "relative" }} id="allInone">
+          <div
+            className="o-container"
+            style={{ position: "relative", paddingLeft: 20, paddingRight: 20 }}
+          >
             <div className="moveLineBox">
               <div className="moveLine" id="lineMove"></div>
             </div>
@@ -245,16 +299,89 @@ export function LandingPage() {
                 style={{ width: "100%" }}
               ></img>
             </div>
+            <motion.div
+              initial={{ x: -1500, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ ease: "easeOut", duration: 4 }}
+              style={{
+                width: 400,
+                border: "1px solid white",
+                position: "absolute",
+                top: 300,
+                left: 0,
+              }}
+            />
+            <motion.div
+              initial={{ y: -700, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ ease: "easeOut", duration: 4 }}
+              style={{
+                borderLeft: "2px solid white" /* 세로 방향 선 속성 */,
+                height: 300 /* 선의 길이 */,
+                margin: 20 /* 여백 설정 */,
+                position: "absolute",
+                top: 0,
+                left: "48%",
+              }}
+            />
+            <motion.div
+              initial={{ x: "100%", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ ease: "easeOut", duration: 4 }}
+              style={{
+                width: 400,
+                border: "1px solid white",
+                position: "absolute",
+                top: 300,
+                right: 0,
+              }}
+            />
+            <motion.div
+              initial={{ y: "100%", opacity: 0 }}
+              animate={{ y: 200, opacity: 1 }}
+              transition={{ ease: "easeOut", duration: 4 }}
+              style={{
+                borderLeft: "2px solid white" /* 세로 방향 선 속성 */,
+                height: 150 /* 선의 길이 */,
+                margin: 20 /* 여백 설정 */,
+                position: "absolute",
+                bottom: 0,
+                left: "48%",
+                zIndex: 10,
+              }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "35%",
+                right: 0,
+              }}
+            >
+              <span
+                style={{ color: "white", fontSize: 45 }}
+                className="typing-text"
+                ref={elementRef}
+              >
+                ## ALL-IN-ONE
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SYSTEM
+              </span>
+            </div>
             <div
               style={{
                 color: "white",
                 display: "flex",
-                justifyContent: "space-evenly",
                 alignItems: "center",
                 marginTop: 200,
               }}
             >
-              <div>
+              <div
+                style={{
+                  position: "absolute",
+                  left: 400,
+                }}
+              >
                 <h2>기획</h2>
                 <article style={{ marginTop: 10 }}>
                   클라이언트와 충분한 협의를
@@ -265,15 +392,14 @@ export function LandingPage() {
                   <br /> 진행합니다
                 </article>
               </div>
-              <div>
-                <h2>기획</h2>
+              <div style={{ position: "absolute", right: 400 }}>
+                <h2>제작</h2>
                 <article style={{ marginTop: 10 }}>
-                  클라이언트와 충분한 협의를
+                  기획된 내용에 따라 다양한
                   <br />
-                  진행한 후 비주얼라온의 크리
+                  프로그램을 활용하여
                   <br />
-                  에이티브를 결합하여 기획을
-                  <br /> 진행합니다
+                  고퀄리티 콘텐츠를 제작합니다
                 </article>
               </div>
             </div>
@@ -281,33 +407,102 @@ export function LandingPage() {
               style={{
                 color: "white",
                 display: "flex",
-                justifyContent: "space-evenly",
                 alignItems: "center",
-                marginTop: 200,
+                marginTop: 400,
               }}
             >
-              <div>
-                <h2>기획</h2>
+              <div
+                style={{
+                  position: "absolute",
+                  left: 400,
+                  zIndex: 10,
+                }}
+              >
+                <h2>설치</h2>
                 <article style={{ marginTop: 10 }}>
-                  클라이언트와 충분한 협의를
+                  설치될 공간과 제작된 콘텐츠
                   <br />
-                  진행한 후 비주얼라온의 크리
+                  에 적합한 장비 , 인테리어등을
                   <br />
-                  에이티브를 결합하여 기획을
-                  <br /> 진행합니다
+                  통해 콘텐츠 활용도를 극대화
+                  <br /> 합니다
                 </article>
               </div>
-              <div>
-                <h2>기획</h2>
+              <div
+                style={{
+                  position: "absolute",
+                  right: 400,
+                  zIndex: 10,
+                }}
+              >
+                <h2>유지보수</h2>
                 <article style={{ marginTop: 10 }}>
-                  클라이언트와 충분한 협의를
+                  설치될 공간과 제작된 콘텐츠
                   <br />
-                  진행한 후 비주얼라온의 크리
+                  에 적합한 장비 , 인테리어등을
                   <br />
-                  에이티브를 결합하여 기획을
-                  <br /> 진행합니다
+                  통해 콘텐츠 활용도를 극대화
+                  <br /> 합니다
                 </article>
               </div>
+            </div>
+          </div>
+        </section>
+
+        <section
+          style={{ width: "100%", position: "relative", marginTop: 200 }}
+        >
+          <div style={{ paddingLeft: 10, paddingRight: 10 }}>
+            <span style={{ color: "white", fontSize: 45 }}>
+              ## OUR
+              <br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CLIENTS
+            </span>
+            <hr color="white" size="1px"></hr>
+          </div>
+          <div className="circleGroup" id="circleEvent">
+            <div id="circle0" className="circle0">
+              <span>문화재청</span>
+            </div>
+
+            <div id="circle1" className="circle0">
+              <span>강화군</span>
+            </div>
+
+            <div id="circle2" className="circle0">
+              <span>김해시</span>
+            </div>
+
+            <div id="circle3" className="circle0">
+              <span>문화재단</span>
+            </div>
+
+            <div id="circle4" className="circle0">
+              <span>인천시</span>
+            </div>
+
+            <div id="circle5" className="circle0">
+              <span>서산시</span>
+            </div>
+
+            <div id="circle6" className="circle0">
+              <span>김해시</span>
+            </div>
+
+            <div id="circle7" className="circle0">
+              <span>문화재청</span>
+            </div>
+
+            <div id="circle8" className="circle0">
+              <span>고흥군</span>
+            </div>
+
+            <div id="circle9" className="circle0">
+              <span>양구군</span>
+            </div>
+
+            <div id="circle10" className="circle0">
+              <span>영동군</span>
             </div>
           </div>
         </section>
